503b0522da4e9023fcb41943ff73f34946e0bdc2
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-05-30 14:00:06 -0700

    semi-finalization of project. Needs work on BG changing according to monster biome before complete.

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // Create MHW retrievable data for every monster.
 // Make menu to select monster, which will change the fetch URL to that specific monster.
-async function FindMonster() {
-    let response = await fetch('https://mhw-db.com/monsters/17');
+let monstObj = {};
+async function FetchMonsterAll() {
+    let response = await fetch(`https://mhw-db.com/monsters/`);
     let data = await response.json();
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].type === "large") {
+            monstObj[i] = [];
+            monstObj[i].push(data[i].id, data[i].name, data[i].locations[0].name, data[i].locations[0].id);
+        }
+    }
+    console.log(monstObj);
+}
+FetchMonsterAll();
+
+async function FetchMonster() {
+    // get selected monster's number from dropdown input.
+    const monsterId = document.getElementsByName("monster-dropdown")[0].value;
+    // fetch data of provided number.
+    let response = await fetch(`https://mhw-db.com/monsters/${monsterId}`);
+    let data = await response.json();
+    // output data as text on right panel.
+    let monsterLoc = monstObj[monsterId][2];
     document.getElementById("name").innerHTML = data.name;
     document.getElementById("species").innerHTML = data.species;
     document.getElementById("description").innerHTML = data.description;
-    console.log(data);
+    document.body.setAttribute("class", monsterLoc);
 }
-
-function changeBG(area) {
-    switch (area) {
-        case 'af':
-            document.body.setAttribute("class", "AF");
-            break;
-        case 'ww':
-            document.body.setAttribute("class", "WW");
-            break;
-        case 'ch':
-            document.body.setAttribute("class", "CH");
-            break;
-        case 'rv':
-            document.body.setAttribute("class", "RV");
-            break;
-        case 'er':
-            document.body.setAttribute("class", "ER");
-            break;
-        default:
-            break;
-    }
+function changeBiome(area) {
+    
 }
