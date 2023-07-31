df8d3338d5417f0af46a20b36b025f5ee1450e18
Khaled Ajaj <khaledajaj@Khaleds-MacBook-Pro.local>
2023-07-31 13:51:27 -0700

    feat: change dropdown menu of monsters to searchbar.

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-// Create MHW retrievable data for every monster.
-// Make menu to select monster, which will change the fetch URL to that specific monster.
+// Object to hold all large monsters.
 let monstObj = {};
+// Create MHW data for every large monster.
+// Used for matching names with id only.
 async function FetchMonsterAll() {
-    let response = await fetch(`https://mhw-db.com/monsters/`);
-    let data = await response.json();
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].type === "large") {
-            monstObj[i] = [];
-            monstObj[i].push(data[i].id, data[i].name, data[i].locations[0].name, data[i].locations[0].id);
-        }
-    }
-    console.log(monstObj);
+	let response = await fetch(`https://mhw-db.com/monsters/`);
+	let data = await response.json();
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].type === 'large') {
+			monstObj[i] = [];
+			monstObj[i].push(
+				data[i].id,
+				data[i].name,
+				data[i].locations[0].name,
+				data[i].locations[0].id
+			);
+		}
+	}
+	console.log(monstObj);
 }
 FetchMonsterAll();
 
+// fetch monster data (name, species, biome/s, description, resistances & weaknesses, rewards).
 async function FetchMonster() {
-    // get selected monster's number from dropdown input.
-    const monsterId = document.getElementsByName("monster-dropdown")[0].value;
-    // fetch data of provided number.
-    let response = await fetch(`https://mhw-db.com/monsters/${monsterId}`);
-    let data = await response.json();
-    // output data as text on right panel.
-    let monsterLoc = monstObj[monsterId][2];
-    document.getElementById("name").innerHTML = data.name;
-    document.getElementById("species").innerHTML = data.species;
-    document.getElementById("description").innerHTML = data.description;
-    document.body.setAttribute("class", monsterLoc);
+	// get monster's name from search input.
+	const monsterName = document.getElementsByName('monster-search')[0].value;
+	// fetch data of provided monster name.
+	const response = await fetch(`https://mhw-db.com/monsters/`);
+	const data = await response.json();
+	let target;
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].name === monsterName) {
+			target = data[i];
+		}
+	}
+	// output data as text on right panel.
+	document.getElementById('name').innerHTML = target.name;
+	document.getElementById('species').innerHTML = target.species;
+	document.getElementById('description').innerHTML = target.description;
 }
-function changeBiome(area) {
-    
-}
+function changeBiome(area) {}
