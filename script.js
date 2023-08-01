81239be8d848c6d20f336b46202e0d229e905dd9
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-08-01 15:44:08 -0700

    feat: add more dynamic bg for all remaining biomes.
    import images of remaining biomes for monsters, create scss classes for each new biome, extend switch statement to hold these new biomes.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let monstObj = new Set();
+async function fetchMonstersAll() {
+	const response = await fetch(`https://mhw-db.com/monsters/`);
+	const data = await response.json();
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].type === 'large' && !monstObj.has(data[i].locations[0])) {
+			monstObj.add(data[i].locations[0].name);
+		}
+	}
+}
+fetchMonstersAll();
+console.log(monstObj);
+
 // get monster's name from search input.
 let searchInput = document.getElementsByName('monster-search');
 
@@ -5,7 +18,6 @@ let searchInput = document.getElementsByName('monster-search');
 window.addEventListener('keydown', (event) => {
 	if (searchInput[0].value && event.key === 'Enter') {
 		FetchMonster();
-		searchInput[0].value = '';
 	}
 });
 
@@ -29,6 +41,7 @@ async function FetchMonster() {
 	document.getElementById('description').innerHTML = target.description;
 	// call function 'changeBiome' with monster's main location as parameter.
 	changeBiome(target.locations[0].name);
+	searchInput[0].value = '';
 }
 
 function changeBiome(area) {
@@ -48,8 +61,23 @@ function changeBiome(area) {
 		case "Elder's Recess":
 			document.body.className = 'er';
 			break;
-		case 'Ancient Forest':
-			document.body.className = 'af';
+		case 'Caverns of El Dorado':
+			document.body.className = 'coed';
+			break;
+		case 'Hoarfrost Reach':
+			document.body.className = 'hr';
+			break;
+		case 'Confluence of Fates':
+			document.body.className = 'cof';
+			break;
+		case 'Secluded Valley':
+			document.body.className = 'sv';
+			break;
+		case 'Guiding Lands':
+			document.body.className = 'gl';
+			break;
+		case 'Great Ravine':
+			document.body.className = 'gr';
 			break;
 
 		default:
